110af74b2b8e42db01a147c4e268374ce313305c
chefburger <burger@pancakeswap.com>
2024-11-21 16:30:02 +0800

    chore: reploy hooks on bsc-test using create3Factory

--- script/01_DeployMockVeToken.s.sol
+++ script/01_DeployMockVeToken.s.sol
@@ -3,22 +3,36 @@ pragma solidity ^0.8.24;
 
 import "forge-std/Script.sol";
 import {BaseScript} from "./BaseScript.sol";
+import {Create3Factory} from "pancake-create3-factory/src/Create3Factory.sol";
 
 import {MockERC20} from "solmate/src/test/utils/mocks/MockERC20.sol";
 
 /**
+ * Step 1: Deploy
  * forge script script/01_DeployMockVeToken.s.sol:DeployMockVeTokenScript -vvv \
  *     --rpc-url $RPC_URL \
  *     --broadcast \
- *     --slow \
- *     --verify
+ *     --slow
+ *
+ * Step 2: Verify
+ * forge verify-contract <address> lib/pancake-v4-universal-router/lib/pancake-v4-periphery/lib/pancake-v4-core/lib/solmate/src/test/utils/mocks/MockERC20.sol:MockERC20 --watch \
+ *      --chain <chain_id> --constructor-args $(cast abi-encode "constructor(string, string, uint256)" "MockVeCake" "VeCake" "18")
  */
 contract DeployMockVeTokenScript is BaseScript {
+    function getDeploymentSalt() public pure override returns (bytes32) {
+        return keccak256("PANCAKE-V4-HOOKS/MockERC20/0.90");
+    }
+
     function run() public {
+        Create3Factory factory = Create3Factory(getAddressFromConfig("create3Factory"));
+
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        MockERC20 VeCake = new MockERC20("MockVeCake", "VeCake", 18);
+        bytes memory creationCode =
+            abi.encodePacked(type(MockERC20).creationCode, abi.encode("MockVeCake", "VeCake", 18));
+        address VeCake = factory.deploy(getDeploymentSalt(), creationCode, keccak256(creationCode), 0, new bytes(0), 0);
+
         emit log_named_address("MockVeCake", address(VeCake));
 
         vm.stopBroadcast();
--- script/02_DeployCLVeCakeExclusiveHook.s.sol
+++ script/02_DeployCLVeCakeExclusiveHook.s.sol
@@ -6,16 +6,27 @@ import {BaseScript} from "./BaseScript.sol";
 
 import {CLVeCakeExclusiveHook} from "../src/pool-cl/vecake-exclusive/CLVeCakeExclusiveHook.sol";
 import {ICLPoolManager} from "pancake-v4-core/src/pool-cl/interfaces/ICLPoolManager.sol";
+import {Create3Factory} from "pancake-create3-factory/src/Create3Factory.sol";
 
 /**
+ * Step 1: Deploy
  * forge script script/02_DeployCLVeCakeExclusiveHook.s.sol:DeployCLVeCakeExclusiveHookScript -vvv \
  *     --rpc-url $RPC_URL \
  *     --broadcast \
- *     --slow \
- *     --verify
+ *     --slow
+ *
+ * Step 2: Verify
+ * forge verify-contract <address> CLVeCakeExclusiveHook --watch \
+ *      --chain <chain_id> --constructor-args $(cast abi-encode "constructor(address, address)" "<clPoolManager> <veCake>")
  */
 contract DeployCLVeCakeExclusiveHookScript is BaseScript {
+    function getDeploymentSalt() public pure override returns (bytes32) {
+        return keccak256("PANCAKE-V4-HOOKS/CLVeCakeExclusiveHook/0.90");
+    }
+
     function run() public {
+        Create3Factory factory = Create3Factory(getAddressFromConfig("create3Factory"));
+
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
@@ -25,8 +36,11 @@ contract DeployCLVeCakeExclusiveHookScript is BaseScript {
         address veCake = getAddressFromConfig("mockVeCake");
         emit log_named_address("VeCake", veCake);
 
-        CLVeCakeExclusiveHook clVeCakeExclusiveHook =
-            new CLVeCakeExclusiveHook(ICLPoolManager(clPoolManager), address(veCake));
+        bytes memory creationCode =
+            abi.encodePacked(type(CLVeCakeExclusiveHook).creationCode, abi.encode(clPoolManager, veCake));
+        address clVeCakeExclusiveHook =
+            factory.deploy(getDeploymentSalt(), creationCode, keccak256(creationCode), 0, new bytes(0), 0);
+
         emit log_named_address("CLVeCakeExclusiveHook", address(clVeCakeExclusiveHook));
 
         vm.stopBroadcast();
